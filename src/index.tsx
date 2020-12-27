735011ea745fcf92076b1a6e959cfa25f1ee6b03
DBankx <61208493+DBankx@users.noreply.github.com>
2020-12-27 10:32:48 +0000

    implemented node-sass and css file structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './application/appLayout/app/App';
 import reportWebVitals from './reportWebVitals';
+import "./assets/sass/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
